ce45aed9bf87ee6b7636405ae1ae09fad5f237a3
Sulea Serban <serban.sulea@ibm.com>
2023-12-31 15:57:52 +0200

    added security and admin dashboard

---
+++ citydangersalert/src/app/services/authentication.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthenticationRequest } from '../models/authentication_request';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+  constructor(private http: HttpClient) {}
+
+  authenticate(authRequest: AuthenticationRequest): Observable<any> {
+    return this.http.post('http://localhost:9090/api/v1/auth/authenticate', authRequest).pipe(
+      map((response: any) => {
+        if (response && response.token) {
+         
+          localStorage.setItem('jwtToken', response.token);
+        }
+        return response;
+      })
+    );
+  }
+}
---
+++ citydangersalert/src/app/services/task-service.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Task } from '../models/task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskServiceService {
+  private apiUrl = 'http://localhost:9090/api/tasks'; 
+
+  constructor(private http: HttpClient) { }
+
+  getAllTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl + '/all');
+  }
+
+  deleteTask(taskId: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${taskId}`);
+  }
+}
